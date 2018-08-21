2edb6bcbb0319f4f0a6c52557dc06747c40f55da
kevinngu <jz_kevin_yu@163.com>
2018-08-20 22:33:23 -0700

    init meterial-ui.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Grid from "@material-ui/core/es/Grid/Grid";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Grid container>
+          <Grid item xs={3} style={{border: '1px solid blue', height: '100%'}}>menu</Grid>
+          <Grid item xs={9} style={{border: '1px solid yellow', height: '100%'}}>document</Grid>
+      </Grid>
     );
   }
 }
